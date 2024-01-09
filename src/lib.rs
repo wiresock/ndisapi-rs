e5587e7004815fb60afebfab73e65a230baf7bfd
Vadim Smirnov <vadim@ntkernel.com>
2024-01-09 14:02:53 +0100

    Resolved Issue #16: Re-export StaticFilter in ndisapi-rs to address missing functionality as reported in https://github.com/wiresock/ndisapi-rs/issues/16.

--- src/lib.rs
+++ src/lib.rs
@@ -28,8 +28,8 @@ pub use crate::ndisapi::{
     DirectionFlags, Eth802_3FilterFlags, EthMRequest, EthPacket, EthRequest, FastIoSection,
     FastIoSectionHeader, FilterFlags, FilterLayerFlags, IcmpFilterFlags, IntermediateBuffer,
     IpV4FilterFlags, IpV6FilterFlags, Ndisapi, NetworkAdapterInfo, PacketOidData, RasLinks,
-    StaticFilterTable, TcpUdpFilterFlags, UnsortedReadSendRequest, Version, ETHER_ADDR_LENGTH,
-    ETH_802_3, FILTER_PACKET_DROP, FILTER_PACKET_DROP_RDR, FILTER_PACKET_PASS,
+    StaticFilter, StaticFilterTable, TcpUdpFilterFlags, UnsortedReadSendRequest, Version,
+    ETHER_ADDR_LENGTH, ETH_802_3, FILTER_PACKET_DROP, FILTER_PACKET_DROP_RDR, FILTER_PACKET_PASS,
     FILTER_PACKET_PASS_RDR, FILTER_PACKET_REDIRECT, ICMP, IPV4, IPV6, IP_RANGE_V4_TYPE,
     IP_RANGE_V6_TYPE, IP_SUBNET_V4_TYPE, IP_SUBNET_V6_TYPE, TCPUDP,
 };
